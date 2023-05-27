338e5dd35aa4db9e0e866c94ec88badd82de2c81
alanagoyal <alanaan@gmail.com>
2023-05-27 17:27:40 +0200

    update og

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -3,39 +3,23 @@ import { ImageResponse } from "@vercel/og"
 export default async function handler() {
   return new ImageResponse(
     (
-      <div>
-        <div
-          style={{
-            fontSize: 80,
-            color: "white",
-            background: "black",
-            width: "100%",
-            height: "100%",
-            padding: "50px 200px",
-            textAlign: "center",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          DocBase 📘
-        </div>
-        <div
-          style={{
-            fontSize: 40,
-            color: "white",
-            background: "black",
-            width: "100%",
-            height: "100%",
-            padding: "50px 200px",
-            textAlign: "center",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          Open-Source Alternative to DocSend
-        </div>
+      <div
+        style={{
+          fontSize: 80,
+          color: "white",
+          background: "black",
+          width: "100%",
+          height: "100%",
+          padding: "50px 200px",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        DocBase 📘
       </div>
     ),
+
     {
       width: 1200,
       height: 630,
