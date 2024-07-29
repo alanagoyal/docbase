b1f22f4830c6f75feef45708495c502e4ce78735
alanagoyal <alanaan@gmail.com>
2024-07-29 09:58:57 -0700

    add error state

--- app/view/[id]/page.tsx
+++ app/view/[id]/page.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link"
 import { createClient } from "@/utils/supabase/server"
-import ViewLinkForm from "@/components/view-link-form"
+
 import { Database } from "@/types/supabase"
+import { Button } from "@/components/ui/button"
+import ViewLinkForm from "@/components/view-link-form"
 
 type Link = Database["public"]["Tables"]["links"]["Row"]
 
@@ -8,19 +11,26 @@ export default async function Doc({ params }: { params: { id: string } }) {
   const supabase = createClient()
   const id = params.id
 
-  const { data: link } = await supabase.rpc("select_link", {
-    link_id: id,
-  }).single() as { data: Link | null };
+  const { data: link } = (await supabase
+    .rpc("select_link", {
+      link_id: id,
+    })
+    .single()) as { data: Link | null }
 
   if (!link) {
-    return <div>Link not found</div>
+    return (
+      <div className="container mx-auto px-4 py-8 flex justify-center items-center flex-col min-h-screen">
+        <h1 className="text-2xl text-center font-bold mb-6">Link not found</h1>
+        <Link href="/">
+          <Button variant="outline">Back Home</Button>
+        </Link>
+      </div>
+    )
   }
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6 text-center">
-        {link.filename}
-      </h1>
+      <h1 className="text-3xl font-bold mb-6 text-center">{link.filename}</h1>
       <ViewLinkForm link={link} />
     </div>
   )
