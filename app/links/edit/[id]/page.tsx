0ea5ae4dac704e7a2dcf80e8e3a916e061278bfc
alanagoyal <alanaan@gmail.com>
2024-08-10 15:30:30 -0700

    fix link error

--- app/links/edit/[id]/page.tsx
+++ app/links/edit/[id]/page.tsx
@@ -1,11 +1,14 @@
-import LinkForm from "@/components/link-form"
+import Link from "next/link"
 import { redirect } from "next/navigation"
 import { createClient } from "@/utils/supabase/server"
+
 import { Database } from "@/types/supabase"
+import { Button } from "@/components/ui/button"
+import LinkForm from "@/components/link-form"
 
 type Link = Database["public"]["Tables"]["links"]["Row"]
 
-export default async function Link({ params }: { params: { id: string } }) {
+export default async function EditLink({ params }: { params: { id: string } }) {
   const id = params.id
   const supabase = createClient()
   const {
@@ -16,9 +19,11 @@ export default async function Link({ params }: { params: { id: string } }) {
     redirect("/login")
   }
 
-  const { data: link } = await supabase.rpc("select_link", {
-    link_id: id,
-  }).single() as { data: Link | null };
+  const { data: link } = (await supabase
+    .rpc("select_link", {
+      link_id: id,
+    })
+    .single()) as { data: Link | null }
 
   const { data: account, error } = await supabase
     .from("users")
@@ -29,7 +34,18 @@ export default async function Link({ params }: { params: { id: string } }) {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-6 text-center">Edit Link</h1>
-      <LinkForm link={link} account={account} />
+      {link ? (
+        <LinkForm link={link} account={account} />
+      ) : (
+        <div className="container mx-auto px-4 py-8 flex justify-center items-center flex-col min-h-screen">
+          <h1 className="text-2xl text-center font-bold mb-6">
+            Oops! This link doesn&apos;t exist
+          </h1>
+          <Link href="/links">
+            <Button variant="outline">Go Back</Button>
+          </Link>
+        </div>
+      )}
     </div>
   )
 }
