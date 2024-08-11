f5f5e37b0c5f0337120e008952ddebc57855370f
alanagoyal <alanaan@gmail.com>
2024-08-11 08:56:15 -0700

    remove unused authuser type

--- types/supabase.ts
+++ types/supabase.ts
@@ -23,35 +23,6 @@ export type ViewerData = {
   viewed_at: string
 }
 
-export type AuthUser = {
-  id: string
-  aud: string
-  role: string
-  email?: string
-  email_confirmed_at?: string
-  phone?: string
-  phone_confirmed_at?: string
-  confirmed_at?: string
-  last_sign_in_at?: string
-  app_metadata: {
-    provider?: string
-    providers?: string[]
-  }
-  user_metadata: Record<string, any>
-  identities?: {
-    id: string
-    user_id: string
-    identity_data: Record<string, any>
-    provider: string
-    last_sign_in_at: string
-    created_at: string
-    updated_at: string
-  }[]
-  created_at: string
-  updated_at: string
-  is_anonymous?: boolean
-}
-
 export type UserInvestment = {
   id: string
   purchase_amount: string | null
